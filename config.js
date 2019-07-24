24236656d0a40ebcbd9c855b14c946c4073ee004
halgai-bori <bori@halgai.com>
2019-07-24 12:54:28 +0800

    config.jp   project.config.json    goods-details  html

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
   version: "1.0.0",
   note: '增加三级分销功能',
   subDomain: "hg", 
-  appid: "wxe4444724a2a56fa4", 
+  appid: "wx01d4f674b090a26e", 
   shareProfile: '百款精品商品，总有一款适合您' 
 }
