7cb8cfe62a1d587df148414964cb2189e652ee99
cr0w <narenhu@halgai.com>
2021-08-18 18:32:37 +0800

    新增功能、新增页面

--- pages/myshop/index.js
+++ pages/myshop/index.js
@@ -4,29 +4,29 @@ Page({
   /**
    * 页面的初始数据
    */
-  data: {
-
+  data: { 
+    typeList:["ᠥᠮᠦᠳᠦ"," ᠳᠡᠪᠡᠯ"," ᠱᠠᠬᠠᠢ"," ᠮᠠᠯᠠᠭᠠᠢ"," ᠬᠦᠵᠦᠭᠦᠪᠴᠢ"],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    var that = this
+    that.picker = that.selectComponent("#custom-picker")
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    
   },
 
   /**
@@ -65,17 +65,66 @@ Page({
   },
   openCamera: function () {
     const that = this;
+    let num = 9
+    if(!that.data.tempFilePaths){
+      
+    }else{
+      num = 9 - that.data.tempFilePaths.length
+    }
     wx.chooseImage({
-      count: 1,
+      count: num,
       sizeType: ['original', 'compressed'],
       sourceType: ['album', 'camera'],
       success (res) {
         // tempFilePath可以作为img标签的src属性显示图片
-        const tempFilePaths = res.tempFilePaths
-        that.setData({
-          tempFilePaths:tempFilePaths
-        })
+        if (!that.data.tempFilePaths){
+          const tempFilePaths = res.tempFilePaths
+          that.setData({
+            tempFilePaths:tempFilePaths
+          })
+        }else{
+          const newTempfilePaths = that.data.tempFilePaths.concat(res.tempFilePaths)
+          that.setData({
+            tempFilePaths:newTempfilePaths
+          })
+        }
       }
+    })
+  },
+  delImage:function(index){
+    const list = this.data.tempFilePaths
+    list.splice(index.currentTarget.dataset.smile,1)
+    this.setData({
+      tempFilePaths:list
+    })
+    
+  },
+  
+  showpicker(){
+    var that = this
+    that.picker.showDialog(that.data.typeList)
+  },
+  _lefttap(){
+    wx.showToast({
+      title: '左边按钮被点击',
+      icon: 'none'
+    })
+  },
+  _righttap(e){
+    var that = this
+    var year = e.detail.year
+    wx.showToast({
+      title: '右边按钮被点击,选择的值为：'+year,
+      icon: 'none'
+    })
+    that.setData({
+      current:year
+    })
+  },
+  _cancel(){
+    wx.showToast({
+      title: 'picker被取消',
+      icon: 'none'
     })
   }
 })
